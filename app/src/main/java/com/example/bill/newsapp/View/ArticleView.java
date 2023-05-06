becbafddfd7f639353d216cc4384730f019d7801
Bill Lugo <striderbillman@gmail.com>
2023-05-06 05:10:20 -0400

    ArticleView - intent set up. Needs more work to get it running. As a temporary measure, clickable link in news items to orignal source.

--- app/src/main/java/com/example/bill/newsapp/View/ArticleView.java
+++ app/src/main/java/com/example/bill/newsapp/View/ArticleView.java
@@ -1,6 +1,61 @@
 package com.example.bill.newsapp.View;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.bill.newsapp.R;
 
 public class ArticleView extends AppCompatActivity {
+    public String TAG = "ArticleView";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.article_view);
+
+        //TODO: fix to get this class to run
+        Log.d(TAG, " starting onCreate() of ArticleView");
+
+
+        String title = getIntent().getExtras().getString("title");
+        String date = getIntent().getExtras().getString("date");
+        String section = getIntent().getExtras().getString("section");
+        String preview = getIntent().getExtras().getString("preview");
+        String author = getIntent().getExtras().getString("author");
+        final String url = getIntent().getExtras().getString("url");
+
+        Log.d(TAG, "title: " + title);
+
+        TextView titleTv = (TextView) findViewById(R.id.title);
+        titleTv.setText(title);
+        TextView tagTv = (TextView) findViewById(R.id.tag);
+        tagTv.setText(section); //TODO: fix inconsistency between tag and section
+        TextView dateTv = (TextView) findViewById(R.id.date);
+        dateTv.setText(date);
+        TextView articleTv = (TextView) findViewById(R.id.article_text);
+        articleTv.setText(preview);
+        TextView authorTv = (TextView) findViewById(R.id.author);
+        authorTv.setText(author);
+        TextView urlTv = (TextView) findViewById(R.id.url);
+        urlTv.setText(url);
+
+
+        urlTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+
+        Log.d(TAG, "intent info. Title: " + title + date + section + preview.substring(0,10) + author + url);
+    }
 }
--- app/src/main/java/com/example/bill/newsapp/View/NewsAdapter.java
+++ app/src/main/java/com/example/bill/newsapp/View/NewsAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +18,15 @@ import com.example.bill.newsapp.R;
 import java.util.List;
 
 public class NewsAdapter extends ArrayAdapter<NewsItem> {
+    public String TAG = "NewsAdapter";
+
     public NewsAdapter(@NonNull Context context, int resource, @NonNull List objects) {
         super(context, resource, objects);
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, @Nullable View convertView, @NonNull final ViewGroup parent) {
         View listItemView = convertView;
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.news_item, parent, false);
@@ -48,15 +51,45 @@ public class NewsAdapter extends ArrayAdapter<NewsItem> {
         TextView preview = (TextView) listItemView.findViewById(R.id.preview);
         preview.setText(currentNewsItem.getPreview());
 
+        TextView urlTv = (TextView) listItemView.findViewById(R.id.url);
+        urlTv.setText(currentNewsItem.getUrl());
+
+        urlTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(currentNewsItem.getUrl()));
+                getContext().startActivity(i);
+            }
+        });
+
         // implement onClickListener to make each newsItem clickable using newsItem's url
         listItemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(currentNewsItem.getUrl()));
+//                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(currentNewsItem.getUrl()));
 
-                //TODO: intent for article view screen (activity?)
+                Log.d(TAG, " this: " + this.toString());
+                Log.d(TAG, " parent.getContext(): " + parent.getContext().getClass().getName());
+                Log.d(TAG, " ArticleView.class: " + ArticleView.class.getName());
+
+                try {
+                Intent i = new Intent(view.getContext(), ArticleView.class);  //???
+                    //TODO: intent for article view screen (activity?)
 //                Intent i = new Intent(this, ArticleView.class);
-                getContext().startActivity(i);
+                    i.putExtra("title", currentNewsItem.getTitle());
+                    i.putExtra("date", currentNewsItem.getPubDate());
+                    i.putExtra("section", currentNewsItem.getSection());
+                    i.putExtra("preview", currentNewsItem.getPreview());
+                    i.putExtra("author", currentNewsItem.getAuthor());
+                    i.putExtra("url", currentNewsItem.getUrl());
+                    Log.d(TAG, " intent testing: " + i.getExtras().getString("url"));
+                    view.getContext().startActivity(i);    //TODO: fix bug triggered here
+                } catch(Exception e){
+                    Log.e(TAG," intent error cause: " + e.getCause());
+                    Log.e(TAG, " intent error msg: " + e.getMessage());
+                }
+
+
             }
         });
 
